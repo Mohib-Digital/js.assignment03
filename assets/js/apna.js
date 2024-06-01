d3e655c053e903889c7a0a2dc95057c996bdb48a
Mohib-Digital <mohibullahafridi2020@gmail.com>
2024-06-01 01:40:01 -0700

    add codes and pic files of chptr 5 and 6

---
+++ assets/js/apna.js
@@ -0,0 +1,162 @@
+// Chptr: Math Expressions
+
+var popularNumber = 4;
+console.log(popularNumber)
+// ================================================================================================
+
+var popularNumber = 2 + 2;
+console.log(popularNumber)
+// ================================================================================================
+
+console.log(2 + 2)
+// ================================================================================================
+
+// alert(2 + 2)
+// ================================================================================================
+
+var num = 20;
+var myNum = num + 100;
+console.log(myNum)
+// ================================================================================================
+
+var oneNum = 12;
+var secondNum = 8;
+var total = oneNum + secondNum;
+console.log(total)
+// ================================================================================================
+
+// + concatenation if any one of the operands is a string
+// var result = 2 + "50"
+// console.log(result)
+
+// var result = "33" + 4 + 4;
+// console.log(result)
+
+// var result = 2 + "27" + 2;
+// console.log(result)
+
+// var result = 4 + 6 + "34";
+// console.log(result)
+
+// var result = 3 + 3 + "1" * 0;
+// console.log(result)
+// ================================================================================================
+
+var fstNum = 12;
+var sndNum = -24;
+var subtractTheNumber = 12 - 24;
+console.log(subtractTheNumber)
+
+// var result = "20" - 15;
+// console.log(result)
+// ================================================================================================
+
+var multiplyTheNumber = 3 * 12;
+console.log(multiplyTheNumber)
+
+// var result = "3" * "9";
+// console.log(result)
+
+// var result = 3 * "34";
+// console.log(result)
+// ================================================================================================
+
+var dividend = 40;
+var divisor = 5;
+var divided = dividend / divisor;
+console.log(divided)
+
+// var divided = 10 / "2";
+// console.log(divided)
+// ================================================================================================
+// modulus operator return remainder
+var dividend = 10;
+var divisor = 3;
+var result = dividend % divisor;
+console.log(result)
+
+// var myModulus = 30 % "10";
+// console.log(myModulus)
+
+// result is "NAN" (infinity)
+// var result = 1 % 0;
+// console.log(result)
+
+// result is zero
+// var result = 0 % 1;
+// console.log(result)
+// ===============================================================================================
+
+var fristNumber = 20;
+var secondNumber = 30;
+var total = fristNumber + secondNumber;
+// to print a message have to used backstage
+console.log(`The sum of ${fristNumber} and ${secondNumber} is: ${total}`)
+// console.log(total)
+// ===============================================================================================
+
+// Chptr:06 :- increment ++ , decrement --
+
+var salary = 99;
+console.log(++salary)
+
+// pre-increment
+// var increment = 9;
+// var newIncrement = ++increment;
+// console.log(newIncrement)
+
+// post-increment
+// var increment = 59;
+// var incremented = increment++;
+// console.log(incremented)
+// console.log(increment)
+// ===============================================================================================
+
+var decrement = 1;
+var newDecrement = --decrement;
+console.log(newDecrement)
+
+// pre-decrement
+// var salary = 100;
+// var decrementedSalary = --salary;
+// console.log(decrementedSalary)
+
+// post-decrement
+// var salary = 100;
+// var decrementedSalary = salary--;
+// console.log(decrementedSalary)
+// console.log(salary)
+// ===============================================================================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
